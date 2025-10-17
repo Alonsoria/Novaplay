c8de63acc18fdfcbd2b38295454823f9e134a098
Alonsoria <alonsoria@outlook.com>
2025-10-17 12:41:26 -0600

    Servidor NovaPlay

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "novaplay"; 
+$servername = "db.fr-pari1.bengt.wasmernet.com";
+$port = 10272;
+$username = "28c30b0b7f68800076e85d6a2f01"; // tu usuario exacto
+$password = "068f28c3-0b0c-7124-8000-2a06da50ca8e";       // pon la contraseña que aparece en Wasmer
+$database = "Novaplay";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Conexión
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
+// Verificar conexión
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
 ?>
